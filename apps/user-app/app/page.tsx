11302a20ba8f5c79f5e849523b13672175673310
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2024-05-28 23:42:07 +0530

    Initial files

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,12 @@
+
+import { Button } from "@repo/ui/button";
+
+
+
+export default function Page(): JSX.Element {
+  return (
+      <div className="text-5xl flex h-screen items-center justify-center">
+        Hi There
+      </div>
+  );
+}
